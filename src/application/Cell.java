d9520bc7a52c3131e7c8f8dc6ab1e93daca9329c
beji <simone0443@outlook.com>
2023-11-25 20:57:21 +0200

    ADDED ANIMATION FOR THE ALGORITHM, BUT THE ALGORITHM IS STILL BORKEN

--- src/application/Cell.java
+++ src/application/Cell.java
@@ -1,41 +1,129 @@
 package application;
 
+import java.util.ArrayList;
+
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-
 public class Cell extends Pane {
 	
-	private boolean isWall = false;
+	private boolean isWall;
 	private boolean isStartCell;
-	private boolean isEndCell;
-	private Cell[] neighbors;
+	private boolean isGoalCell;
+	private boolean isInOpenSet;
+	private boolean isInClosedSet;
+	//Parent node: reference to the node that was expanded to generate the current node.
+	private Cell parentNode;
+	private double x;
+	private double y;
+	private int i;
+	private int j;
+	//G-Cost: the cost of getting from the start node to this node.
+	public double gCost;
+	//H-Cost(Heuristics cost): the cost of getting from this node to the goal node.
+	public double hCost;
+	//F-Cost: the total cost, which is the sum of the G-Cost and the H-Cost.
+	public double fCost;
 	
-	public Cell()
+	
+	private ArrayList<Cell> neighbors = new ArrayList<Cell>();
+	
+	public Cell(int i, int j)
 	{
+		this.i = i;
+		this.j = j;
 		
+		x = getLayoutX();
+		y = getLayoutY();
+		
+		gCost = 0.0;
+		hCost = 0.0;
+		fCost = 0.0;
+		parentNode = null;
 	}
-		
+	
+	public void addNeighbor(Cell c)
+	{
+		neighbors.add(c);
+	}
+	
+	public ArrayList<Cell> getNeighbors()
+	{
+		return neighbors;
+	}
+	
+	public void calculateFCost()
+	{
+		fCost = gCost + hCost;
+	}
+	
+	public int getI()
+	{
+		return i;
+	}
+	
+	public int getJ()
+	{
+		return j;
+	}
+	
+	public double getX()
+	{
+		return x;
+	}
+	
+	public double getY()
+	{
+		return y;
+	}
+
+	public Cell getParentNode()
+	{
+		return parentNode;
+	}
+	
 	public boolean isStartCell()
 	{
 		return isStartCell;
 	}
 		
-	public boolean isEndCell()
+	public boolean isGoalCell()
 	{
-		return isEndCell;
+		return isGoalCell;
 	}
-		
+	
+	public void isInOpenSet(boolean value)
+	{
+		isInOpenSet = value;
+		updateFill();
+	}
+	
+	public boolean isInOpenSet()
+	{
+		return isInOpenSet;
+	}
+	
+	public void isInClosedSet(boolean value)
+	{
+		isInClosedSet = value;
+		updateFill();
+	}
+	
+	public boolean isInClosedSet()
+	{
+		return isInClosedSet;
+	}
+	
 	public void setStartCell(boolean isStartCell)
 	{
 		this.isStartCell = isStartCell; 
 		updateFill();
 	}
 		
-	public void setEndCell(boolean isEndCell)
+	public void setGoalCell(boolean isGoalCell)
 	{
-		this.isEndCell = isEndCell; 
+		this.isGoalCell = isGoalCell; 
 		updateFill();
 	}
 		
@@ -63,7 +151,7 @@ public class Cell extends Pane {
 			c.setFill(Color.GREEN);
 			getChildren().add(c);
 	
-	        } else if (isEndCell) {
+	        } else if (isGoalCell) {
 				//setColor("red");
 				Circle c = new Circle();
 				c.setRadius(7);
@@ -84,7 +172,11 @@ public class Cell extends Pane {
 				
 				getChildren().add(c);
 				// or any other color for regular cells
-	        } else {
+	        } else if(isInOpenSet) {
+	        	setStyle("-fx-background-color: green");
+			} else if(isInClosedSet){ 
+	        	setStyle("-fx-background-color: red");
+			} else {
 	        	getChildren().clear();
 	        }
     }
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,15 +1,20 @@
 package application;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.ResourceBundle;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.event.EventTarget;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -20,8 +25,13 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class MainController implements Initializable
 {
@@ -55,7 +65,7 @@ public class MainController implements Initializable
 	
 	private Cell startCell;
 	
-	private Cell endCell;
+	private Cell goalCell;
 	
 	private static final int ROWS = 40;
     private static final int COLS = 60;
@@ -67,7 +77,15 @@ public class MainController implements Initializable
 	private boolean bShowGridLines = false;
 	
 	private Tooltip tt = new Tooltip();
+	
+	private ArrayList<Cell> openSet = new ArrayList<Cell>();
+	//openSet stores all the nodes that still need to be evaluated.
 
+	private ArrayList<Cell> closedSet = new ArrayList<Cell>();
+	//closedSet stores all the nodes that finished being evaluated.
+	private Timeline timeline = new Timeline();
+
+	private int pointTimeline = 0;
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1)
 	{
@@ -100,53 +118,184 @@ public class MainController implements Initializable
 		cells = new Cell[ROWS][COLS];
 		
 
-		for(int i = 0; i < 40; i++)
+		for(int i = 0; i < ROWS; i++)
 		{
-			for(int j = 0; j < 60; j++)
+			for(int j = 0; j < COLS; j++)
 			{	
 				Cell cell;
-				cell = new Cell();
+				cell = new Cell(i, j);
 				
 				cells[i][j] = cell;
 				grid.add(cell, j, i);
 			}
 		}
 		
+		for(int i = 0; i < ROWS; i++)
+		{
+			for(int j = 0; j < COLS; j++)
+			{	
+				if(i < ROWS - 1)
+				{
+					cells[i][j].addNeighbor(cells[i + 1][j]);
+				}
+				if(i > 0)
+				{
+					cells[i][j].addNeighbor(cells[i - 1][j]);
+				}
+				if(j < COLS - 1)
+				{
+					cells[i][j].addNeighbor(cells[i][j + 1]);
+				}
+				if(j > 0)
+				{
+					cells[i][j].addNeighbor(cells[i][j - 1]);
+				}	
+			}
+		}
+		
+		for(int i = 0; i < ROWS; i++)
+		{
+			for(int j = 0; j < COLS; j++)
+			{
+				System.out.printf("Node (%d, %d): [", i, j);
+				for(Cell c : cells[i][j].getNeighbors())
+				{
+					System.out.print(" (" + c.getI() + ", " + c.getJ() + ") ");
+				}
+				System.out.println("]");
+			}
+		}
 	}
 	
-	public void executeAlgo()
+	
+	public double calculateManhattanDistance(Cell sourceCell, Cell destCell)
 	{
-//		Path path = new Path();
-//		path.setStrokeWidth(3);
-//		path.setStroke(Color.CORNFLOWERBLUE);
-//		path.getElements().add(new MoveTo(400, 50));
-//		path.getElements().add(new LineTo(1000, 300));
-//		
-//		grid.getChildren().add(path);
-		
+		return Math.abs(sourceCell.getX() - destCell.getX()) + Math.abs(sourceCell.getY() - destCell.getY()); 
+	}
+	
+	public void run()
+	{
+		aStar();
+		timeline.play();
+	}
+	
+	public void aStar()
+	{
+
 		//actually use background colors for the path
-		for(int i = 0; i < ROWS / 2; i++)
+		for(int i = 0; i < ROWS ; i++)
 		{
-			for(int j = 0; j < COLS /2; j++)
+			for(int j = 0; j < COLS ; j++)
 			{
 				if(cells[i][j].isStartCell())
 				{
 					startCell = cells[i][j];
 				}
-				if(cells[i][j].isEndCell())
+				if(cells[i][j].isGoalCell())
 				{
-					endCell = cells[i][j];
+					goalCell = cells[i][j];
 				}
 
-				System.out.printf("Cells(i: %d, j: %d): \n", i, j);
-				System.out.printf("layout x: %.2f\n",cells[i][j].getLayoutX());
-				System.out.printf("layout y: %.2f\n",cells[i][j].getLayoutY());
-
+				//System.out.printf("Cells(i: %d, j: %d): \n", i, j);
+				//System.out.printf("layout x: %.2f\n",cells[i][j].getLayoutX());
+				//System.out.printf("layout y: %.2f\n",cells[i][j].getLayoutY());
+					
 			}
 		}
 		
-		System.out.println(startCell);
-		System.out.println(endCell);
+		if(startCell != null && goalCell != null)
+		{
+			//System.out.println(startCell);
+			//System.out.println(goalCell);	
+
+			openSet.add(startCell);
+			KeyFrame kf = new KeyFrame(Duration.millis(pointTimeline * 10), e -> {
+				startCell.isInOpenSet(true);
+			});
+			pointTimeline++;
+			
+			timeline.getKeyFrames().add(kf);
+			//startCell.isInOpenSet(true);
+			
+			while(!openSet.isEmpty())
+			{
+				
+				int winner = 0;
+				for(int i = 0; i < openSet.size(); i++)
+				{
+					if(openSet.get(i).fCost < openSet.get(winner).fCost)
+					{
+						winner = i;
+					}
+				}
+				
+				var current = openSet.get(winner);
+				
+				if(current == goalCell)
+				{
+					System.out.println("DONE! UwU");
+				}
+				
+				openSet.removeIf(c -> c.equals(current));
+				
+				kf = new KeyFrame(Duration.millis(pointTimeline * 10), e -> {
+					current.isInOpenSet(false);
+				});
+				pointTimeline++;
+				
+				timeline.getKeyFrames().add(kf);
+				//current.isInOpenSet(false);
+				
+				//check if it removes the right thing
+				closedSet.add(current);
+				
+				kf = new KeyFrame(Duration.millis(pointTimeline * 10), e -> {
+					current.isInClosedSet(true);
+				});
+				pointTimeline++;
+				
+				timeline.getKeyFrames().add(kf);
+				//current.isInClosedSet(true);
+				
+				ArrayList<Cell> neighbors = current.getNeighbors();
+				
+				for(int i = 0; i < neighbors.size(); i ++)
+				{
+					Cell neighbor = neighbors.get(i);
+					
+					if(!closedSet.contains(neighbor))
+					{
+						double tempG = current.gCost + 1;
+						
+						//current.gCost = calculateManhattanDistance(startCell, current);
+						if(openSet.contains(neighbor))
+						{
+							if(tempG < neighbor.gCost)
+							{
+								neighbor.gCost = tempG;
+							}
+						}
+						else 
+						{
+							neighbor.gCost = tempG;
+							
+							kf = new KeyFrame(Duration.millis(pointTimeline * 10), e -> {
+								neighbor.isInOpenSet(true);
+							});
+							pointTimeline++;
+							
+							timeline.getKeyFrames().add(kf);
+							//neighbor.isInOpenSet(true);
+							openSet.add(neighbor);
+						}
+					}
+					
+					neighbor.hCost = calculateManhattanDistance(neighbor, goalCell);
+					neighbor.fCost = neighbor.gCost + neighbor.hCost;
+				}
+				
+			}
+		}
 	}
 	
 	public void showGridLines()
@@ -157,12 +306,12 @@ public class MainController implements Initializable
 	
 	public void generateRandomWalls()
 	{
-
+		//TODO: animate this bullshit
 		for(int i = 0; i < ROWS; i++)
 		{
 			for(int j = 0; j < COLS; j++)
 			{ 
-				if(!cells[i][j].isStartCell() && !cells[i][j].isEndCell())
+				if(!cells[i][j].isStartCell() && !cells[i][j].isGoalCell())
 				{
 					if(r.nextFloat(1) < 0.3)
 					{
@@ -190,7 +339,7 @@ public class MainController implements Initializable
 			for(int j = 0; j < COLS; j++)
 			{ 
 				if(!cells[i][j].isStartCell() && !cells[i][j].isStartCell() &&
-				   !cells[i][j].isEndCell() && !cells[i][j].isEndCell()	)
+				   !cells[i][j].isGoalCell() && !cells[i][j].isGoalCell())
 				{
 					cells[i][j].isWall(false);
 				}
@@ -203,7 +352,7 @@ public class MainController implements Initializable
 			
 			if(n < 2)
 			{	
-				if(!cells[i][0].isStartCell() && !cells[i][0].isEndCell())
+				if(!cells[i][0].isStartCell() && !cells[i][0].isGoalCell())
 				cells[i][0].isWall(true);
 
 			}
@@ -211,7 +360,7 @@ public class MainController implements Initializable
 			n = r.nextInt(4);
 			if(n < 2)
 			{
-				if(!cells[i][COLS - 1].isStartCell() && !cells[i][COLS - 1].isEndCell())
+				if(!cells[i][COLS - 1].isStartCell() && !cells[i][COLS - 1].isGoalCell())
 				{
 					cells[i][COLS - 1].isWall(true);
 				}
@@ -226,14 +375,14 @@ public class MainController implements Initializable
 				{
 					if(r.nextInt(2) == 0)
 					{
-						if(!cells[i][j - 1].isStartCell() && !cells[i][j - 1].isEndCell())
+						if(!cells[i][j - 1].isStartCell() && !cells[i][j - 1].isGoalCell())
 						{
 							cells[i][j - 1].isWall(true);
 						}
 					}
 					else
 					{
-						if(!cells[i + 1][j].isStartCell() && !cells[i + 1][j].isEndCell())
+						if(!cells[i + 1][j].isStartCell() && !cells[i + 1][j].isGoalCell())
 						{
 							cells[i + 1][j].isWall(true);
 						}
@@ -251,7 +400,7 @@ public class MainController implements Initializable
 			{
 				cells[i][j].isWall(false);
 				cells[i][j].setStartCell(false);
-				cells[i][j].setEndCell(false);
+				cells[i][j].setGoalCell(false);
 			}
 		} 
 	}
@@ -298,14 +447,14 @@ public class MainController implements Initializable
 								Cell parentCell = (Cell)c.getParent();
 								parentCell.isWall(false);
 								parentCell.setStartCell(false);
-								parentCell.setEndCell(false);
+								parentCell.setGoalCell(false);
 							}
 							if(ev.getPickResult().getIntersectedNode().toString().contains("Cell"))
 							{
 								Cell cell = (Cell)ev.getPickResult().getIntersectedNode();
 								cell.isWall(false);
 								cell.setStartCell(false);
-								cell.setEndCell(false);
+								cell.setGoalCell(false);
 							}
 						}
 						catch(Exception e)
@@ -366,15 +515,15 @@ public class MainController implements Initializable
 					{
 						for(int j = 0; j < COLS; j++)
 						{
-							if(cells[i][j].isEndCell())
+							if(cells[i][j].isGoalCell())
 							{
-								cells[i][j].setEndCell(false);
+								cells[i][j].setGoalCell(false);
 							}
 						}
 					}
 					
 					Cell c = (Cell)et;
-					c.setEndCell(true);
+					c.setGoalCell(true);
 				}
 			}
 			if(ev.getButton() == MouseButton.SECONDARY) //DELETUS TYPE OF NODUS
@@ -385,13 +534,13 @@ public class MainController implements Initializable
 					Cell parentCell = (Cell)c.getParent();
 					parentCell.isWall(false);
 					parentCell.setStartCell(false);
-					parentCell.setEndCell(false);
+					parentCell.setGoalCell(false);
 				}
 				if(et.toString().contains("Cell"))
 				{
 					cell.isWall(false);
 					cell.setStartCell(false);
-					cell.setEndCell(false);
+					cell.setGoalCell(false);
 				}
 			}
 		}	
